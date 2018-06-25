4c4ac9b617cec8e02e51c6f070c40a63c81302c9
sumanajo <40545554+sumanajo@users.noreply.github.com>
2018-06-26 06:41:58 +0700

    Update elist.sh

--- elist.sh
+++ elist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen
-rm -rf cpuminer-opt
+sudo killall cpuminer
+sudo rm -rf cpuminer-opt
 git clone https://github.com/JayDDee/cpuminer-opt
 cd cpuminer-opt
 ./build.sh
 screen -S dao
-./cpuminer -a lyra2z330 -o stratum+tcp://www.zoin.biz:3032 -u excalibur.azu -p x
-
+screen -d -m ./cpuminer -a lyra2z330 -o stratum+tcp://www.zoin.biz:3032 -u excalibur.azu -p x
